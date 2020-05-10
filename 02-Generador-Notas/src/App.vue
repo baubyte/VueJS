d788664e58f06bead3f491f406acaff893f61569
BAUBYTE <paredbaez.martin@gmail.com>
2020-05-09 22:08:05 -0300

    Generador de Notas Inciando

--- 02-Generador-Notas/src/App.vue
+++ 02-Generador-Notas/src/App.vue
@@ -1,60 +1,36 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h3>{{ titulo }}</h3>
+    <div class="form">
+      <div class="form-group">
+        <label>Titulo</label>
+        <input class="form-control" type="text" v-model="nota.titulo" />
+      </div>
+      <div class="form-group">
+        <label>Texto</label>
+        <textarea class="form-control" v-model="nota.texto"></textarea>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      titulo: "Gestion de Notas",
+      nota: {
+        titulo: "",
+        texto: ""
+      }
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
+.form {
+  text-align: left;
 }
 </style>
