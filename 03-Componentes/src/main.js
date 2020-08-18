7f64547e4e19fbf6196eafc8e307b241d5f11ccc
baubyte <paredbaez.martin@gmail.com>
2020-08-17 21:18:52 -0300

    componenetes en linea

--- 03-Componentes/src/main.js
+++ 03-Componentes/src/main.js
@@ -3,10 +3,20 @@ import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
-
-Vue.component('saludo', {
+    /**Componentes (Extendiendo)*/
+var componente1 = Vue.extend({
     template: '<h1>Hola - Introducción a Componentes.</h1>'
-});
+})
+var componente2 = Vue.extend({
+    template: '<h3>Listado de Frameworks JavaScripts.</h3>'
+})
+var componente3 = Vue.extend({
+    template: '<ul><li>Vue.js</li><li>Angular</li><li>React</li></ul>'
+})
+Vue.component('componente-en-linea', {});
+Vue.component('componente-saludo', componente1);
+Vue.component('componente-titulo', componente2);
+Vue.component('componente-lista', componente3);
 new Vue({
     el: '#app'
 })
