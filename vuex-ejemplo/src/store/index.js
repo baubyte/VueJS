4c483e98130f8d39d57f578f74c6092870034ebf
BAUBYTE <paredbaez.martin@gmail.com>
2020-09-29 23:10:37 -0300

    store y getters

--- vuex-ejemplo/src/store/index.js
+++ vuex-ejemplo/src/store/index.js
@@ -5,11 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    msg:'Vuex Desde Cero.',
+    nombre:'BAU',
+    apellido:'BYTE'
   },
   mutations: {
   },
   actions: {
   },
   modules: {
+  },
+  getters:{
+    mensaje(state){
+      return state.msg;
+    },
+    nombreCompleto(state){
+      return state.nombre+state.apellido;
+    }
+
   }
 })
